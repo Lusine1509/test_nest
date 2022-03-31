fce7626cfb5ab52f9cad8db414f2dd28e4a726bf
Lusine Andriasyan <lusine_andriasyan@epam.com>
2022-03-31 13:29:06 +0400

    adding winstonmodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,44 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AppController } from './app.controller';
 import { HealthModule } from './health/health.module';
+import { WinstonModule } from 'nest-winston';
+import * as winston from 'winston';
+
+
+const { label, json, timestamp, align, printf } = winston.format
+
+const myFormat = printf(
+  ({ level, message, label, timestamp }) => {
+      return `{\n\tlabel: ${label},\n\ttimestamp: ${timestamp},\n\tlevel: ${level},\n\tmessage: ${message}\n},`
+  }
+)
 
 @Module({
-  imports: [HealthModule],
+  imports: [
+    WinstonModule.forRoot({
+      levels: {
+        error: 0,
+        debug: 1,
+        warn: 2,
+        data: 3,
+        info: 4,
+        verbose: 5,
+        silly: 6,
+        custom: 7,
+    },
+    format: winston.format.combine(
+        label({ label: '🚩 Info!' }),
+        json(),
+        timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+        align(),
+        myFormat
+    ),
+    transports: [new winston.transports.Console()]
+  
+    }),
+    HealthModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
